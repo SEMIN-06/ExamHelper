ac488948e93c9b6ee95db7385c854a391110157e
JustURA <activate@kakao.com>
2022-12-11 16:18:49 +0900

    숫자 기호 50까지 추가

--- src/renderer/pages/Print.tsx
+++ src/renderer/pages/Print.tsx
@@ -7,7 +7,7 @@ import styled from 'styled-components';
 import { useReactToPrint } from 'react-to-print';
 import LoadingSpinner from '../components/LoadingSpinner';
 
-const NumberCircles = [ "①", "②", "③", "④", "⑤", "⑥", "⑦", "⑧", "⑨", "⑩", "⑪", "⑫", "⑬", "⑭", "⑮" ]
+const NumberCircles = [ "①", "②", "③", "④", "⑤", "⑥", "⑦", "⑧", "⑨", "⑩", "⑪", "⑫", "⑬", "⑭", "⑮", "⑯", "⑰", "⑱", "⑲", "⑳", "㉑", "㉒", "㉓", "㉔", "㉕", "㉖", "㉗", "㉘", "㉙", "㉚", "㉛", "㉜", "㉝", "㉞", "㉟", "㊱", "㊲", "㊳", "㊴", "㊵", "㊶", "㊷", "㊸", "㊹", "㊺", "㊻", "㊼", "㊽", "㊾", "㊿" ]
 
 const PrintWrapper = styled.div<{zoomLevel: number}>`
   display: flex;
